6077000fa60f86906a638a724b32dc575c900ec6
月海 <spectres@foxmail.com>
2021-07-19 18:45:47 +0800

    add insert logic

--- src/one-million/one-million.service.ts
+++ src/one-million/one-million.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { OneMillion, OneMillionDocument } from './schemas/one-million.schemas';
 
 @Injectable()
-export class OneMillionService {
+export class OneMillionService implements OnModuleInit {
   constructor(@InjectModel(OneMillion.name) private readonly oneMillionModel: Model<OneMillionDocument>) {}
+
+  async onModuleInit() {
+    const totalDocuments = await this.oneMillionModel.countDocuments();
+    const totalCount = 1000000;
+    const diff = totalCount - totalDocuments;
+    if (diff === 0) return;
+    if (diff > 0) {
+      const docList = Array.from({ length: diff }, v => (v = { name: '一百万' }));
+      await this.oneMillionModel.insertMany(docList);
+    } else {
+      for (let i = 0; i < -diff; i++) {
+        await this.oneMillionModel.deleteOne({});
+      }
+    }
+  }
 
   async createOne(name: string, age?: number, gender?: string, description?: string): Promise<OneMillionDocument> {
     const doc = Object.assign({ name }, age ? { age } : {}, gender ? { gender } : {}, description ? { description } : {});
--- src/ten-million/ten-million.service.ts
+++ src/ten-million/ten-million.service.ts
@@ -1,14 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { TenMillion, TenMillionDocument } from './schemas/ten-million.schema';
 
 @Injectable()
-export class TenMillionService {
+export class TenMillionService implements OnModuleInit {
   constructor(
     @InjectModel(TenMillion.name)
     private readonly tenMillionModel: Model<TenMillionDocument>
   ) {}
+
+  async onModuleInit() {
+    const totalDocuments = await this.tenMillionModel.countDocuments();
+    const totalCount = 10000000;
+    const oneBatch = 100000;
+    const diff = totalCount - totalDocuments;
+    if (diff === 0) return;
+    if (diff > 0) {
+      const mod = diff % oneBatch;
+      const circles = Math.floor(diff / oneBatch);
+      for (let i = 0; i < circles; i++) {
+        const docList = [];
+        for (let j = 0; j < oneBatch; j++) {
+          docList.push({ name: '一千万' });
+        }
+        console.log(i);
+        const result = await this.tenMillionModel.insertMany(docList);
+        console.log('插入一批');
+      }
+      // const docList = Array.from({ length: mod }, v => (v = { name: '一千万' }));
+      const docList = [];
+      for (let j = 0; j < mod; j++) {
+        docList.push({ name: '一千万' });
+      }
+      await this.tenMillionModel.insertMany(docList);
+    } else {
+      for (let i = 0; i < -diff; i++) {
+        await this.tenMillionModel.deleteOne({});
+      }
+    }
+  }
 
   async createOne(name: string, age?: number, gender?: string, description?: string): Promise<TenMillionDocument> {
     const doc = Object.assign({ name }, age ? { age } : {}, gender ? { gender } : {}, description ? { description } : {});
